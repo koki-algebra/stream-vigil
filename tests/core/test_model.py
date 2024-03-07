ec901da973ece8d718a45c7f6b282fa8fac4e51c
koki-algebra <yotsuba00040@gmail.com>
2024-03-08 03:29:14 +0900

    remove property

--- tests/core/test_model.py
+++ tests/core/test_model.py
@@ -18,15 +18,3 @@ def model(mock_auto_encoder) -> Model:
 def test_model_id(model: Model):
     assert model.model_id is not None
     assert isinstance(model.model_id, uuid.UUID)
-
-
-def test_reliability_setter_valid(model: Model):
-    model.reliability = 0.8
-    assert model.reliability == 0.8
-
-
-def test_reliability_setter_invalid(model: Model):
-    with pytest.raises(ValueError):
-        model.reliability = -0.5
-    with pytest.raises(ValueError):
-        model.reliability = 1.5

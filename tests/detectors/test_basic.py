e7e33937caea1b9a61e3c10a1c6c2e4eb8575fcf
koki-algebra <yotsuba00040@gmail.com>
2024-03-17 19:22:17 +0900

    add validate scores

--- tests/detectors/test_basic.py
+++ tests/detectors/test_basic.py
@@ -1,5 +1,9 @@
+import uuid
+
+import pytest
 import torch
 
+from streamvigil.core.utils import validate_anomaly_scores
 from streamvigil.detectors import BasicDetector
 from tests.mock import MockAutoEncoder
 
@@ -7,8 +11,48 @@ from tests.mock import MockAutoEncoder
 auto_encoder = MockAutoEncoder(input_dim=10, hidden_dim=8, latent_dim=5)
 
 
+def test_model_id():
+    detector = BasicDetector(auto_encoder)
+
+    assert detector.model_id is not None
+    assert isinstance(detector.model_id, uuid.UUID)
+
+    # set new id
+    detector.model_id = uuid.uuid4()
+
+
+def test_reliability():
+    detector = BasicDetector(auto_encoder)
+    with pytest.raises(ValueError):
+        detector._set_reliability(-1.5)
+    with pytest.raises(ValueError):
+        detector._set_reliability(1.5)
+
+
+def test_update_reliability():
+    detector = BasicDetector(auto_encoder)
+
+    valid_scores = torch.rand(10)
+    detector.update_reliability(valid_scores)
+
+    invalid_scores = torch.Tensor([-1.0, 0.5, 0.2, 1.5, -0.2])
+    with pytest.raises(ValueError):
+        detector.update_reliability(invalid_scores)
+
+
+def test_update_last_batch_scores():
+    detector = BasicDetector(auto_encoder)
+
+    valid_scores = torch.rand(10)
+    detector.update_last_batch_scores(valid_scores)
+
+    invalid_scores = torch.Tensor([-1.0, 0.5, 0.2, 1.5, -0.2])
+    with pytest.raises(ValueError):
+        detector.update_last_batch_scores(invalid_scores)
+
+
 def test_predict():
     detector = BasicDetector(auto_encoder)
     x = torch.randn(64, 10)
     scores = detector.predict(x)
-    assert ((scores >= 0.0) & (scores <= 1.0)).all().item()
+    assert validate_anomaly_scores(scores)

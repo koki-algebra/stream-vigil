cc3e179fa3e6190afda80fe42e6e418902ca104d
koki-algebra <yotsuba00040@gmail.com>
2024-03-21 00:35:45 +0900

    fix ARCUS

--- streamvigil/_arcus.py
+++ streamvigil/_arcus.py
@@ -13,7 +13,7 @@ class ARCUS:
     def __init__(
         self, detector: AnomalyDetector, reliability_threshold=0.95, similarity_threshold=0.8, max_model_num=5
     ) -> None:
-        self.pool = ModelPool(detector, reliability_threshold, similarity_threshold, max_model_num)
+        self._pool = ModelPool(detector, reliability_threshold, similarity_threshold, max_model_num)
         self._is_init = False
 
     def init(self, x: torch.Tensor) -> None:
@@ -21,9 +21,9 @@ class ARCUS:
             return
 
         # Add initial model
-        model_id = self.pool.add_model()
+        model_id = self._pool.add_model()
         # Train the new model
-        self.pool.train(model_id, x)
+        self._pool.train(model_id, x)
 
         self._is_init = True
 
@@ -42,22 +42,22 @@ class ARCUS:
             Anomaly scores on the data matrix X.
         """
         # Estimate the anomaly scores
-        scores = self.pool.predict(x)
+        scores = self._pool.predict(x)
 
-        if self.pool.is_drift():
+        if self._pool.is_drift():
             # Add new model
-            model_id = self.pool.add_model()
+            model_id = self._pool.add_model()
             # Train the new model
-            self.pool.train(model_id, x)
+            self._pool.train(model_id, x)
 
             # Compress the model pool
-            while self.pool.compress(x, model_id):
+            while self._pool.compress(x, model_id):
                 continue
         else:
             # Find the most reliable model in the model pool
             model_id = self._find_most_reliable_model()
             # Train the model
-            self.pool.train(model_id, x)
+            self._pool.train(model_id, x)
 
         return scores
 
@@ -66,7 +66,7 @@ class ARCUS:
         Find the most reliable model in the model pool.
         """
         max_reliability = -1.0
-        models = self.pool.get_models()
+        models = self._pool.get_models()
         for model in models:
             if max_reliability < model.reliability:
                 max_id = model.model_id

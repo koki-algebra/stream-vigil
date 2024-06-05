961aa6c0c1da1a3f135cc3d1b845c7d17541c540
koki-algebra <biz.kokiyamada1022@gmail.com>
2024-06-05 13:58:21 +0900

    add AUPRC

--- examples/rapp.py
+++ examples/rapp.py
@@ -2,7 +2,7 @@ from logging import getLogger
 from logging.config import dictConfig
 
 from torch.utils.data import DataLoader
-from torcheval.metrics import BinaryAUROC
+from torcheval.metrics import BinaryAUPRC, BinaryAUROC
 from yaml import safe_load
 
 from streamvigil import CSVDataset
@@ -60,15 +60,21 @@ def main():
     logger.info("Completed training the model!")
 
     # Evaluation
-    metrics = BinaryAUROC()
     logger.info("Start evaluating the model...")
+
+    # Area Under the ROC Curve
+    auroc = BinaryAUROC()
+    # Area Under the Precision-Recall Curve
+    auprc = BinaryAUPRC()
 
     for X, y in test_loader:
         scores = detector.predict(X)
-        metrics.update(scores, y)
+        auroc.update(scores, y)
+        auprc.update(scores, y)
 
-    # Compute AUROC score
-    logger.info(f"AUROC Score: {metrics.compute()}")
+    # Compute evaluation scores
+    logger.info(f"AUROC Score: {auroc.compute()}")
+    logger.info(f"AUPRC Score: {auprc.compute()}")
 
     logger.info("Completed the evaluation of the model!")
 

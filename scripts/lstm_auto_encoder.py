33ddcd49af4a518316bd0ea1e9105e170251a588
Koki Yamada <54975589+koki-algebra@users.noreply.github.com>
2024-06-26 17:00:09 +0900

    add LSTM AutoEncoder (#100)

--- scripts/lstm_auto_encoder.py
+++ scripts/lstm_auto_encoder.py
@@ -1,135 +1,11 @@
-from typing import Tuple
-
-import numpy as np
-import pandas as pd
 import torch
-from sklearn.model_selection import train_test_split
 from torch import nn, optim
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
 from torcheval.metrics import BinaryAUPRC, BinaryAUROC
 
+from streamvigil import CSVDataset
+from streamvigil.detectors import LSTMAutoEncoder
 from streamvigil.utils import set_seed
-
-
-class LSTMEncoder(nn.Module):
-    def __init__(
-        self,
-        input_size: int,
-        hidden_size: int,
-        num_layers: int,
-    ) -> None:
-        super().__init__()
-
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-
-        self.lstm = nn.LSTM(
-            input_size,
-            hidden_size,
-            num_layers,
-            batch_first=True,
-        )
-
-    def forward(self, x: torch.Tensor):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        _, (hn, cn) = self.lstm(x, (h0, c0))
-
-        return hn, cn
-
-
-class LSTMDecoder(nn.Module):
-    def __init__(
-        self,
-        input_size: int,
-        hidden_size: int,
-        num_layers: int,
-        output_size: int,
-    ) -> None:
-        super().__init__()
-
-        self.lstm = nn.LSTM(
-            input_size,
-            hidden_size,
-            num_layers,
-            batch_first=True,
-        )
-        self.fc = nn.Linear(hidden_size, output_size)
-
-    def forward(self, x: torch.Tensor, hn: int, cn: int):
-        out, _ = self.lstm(x, (hn, cn))
-        out = self.fc(out)
-
-        return out
-
-
-class LSTMAutoEncoder(nn.Module):
-    def __init__(
-        self,
-        input_size: int,
-        hidden_size: int,
-        num_layers: int,
-    ) -> None:
-        super().__init__()
-        self.encoder = LSTMEncoder(
-            input_size,
-            hidden_size,
-            num_layers,
-        )
-        self.decoder = LSTMDecoder(
-            input_size,
-            hidden_size,
-            num_layers,
-            input_size,
-        )
-
-    def forward(self, x: torch.Tensor):
-        hn, cn = self.encoder(x)
-        decoder_input = torch.zeros(x.size(0), x.size(1), x.size(2)).to(x.device)
-        out = self.decoder(decoder_input, hn, cn)
-
-        return out
-
-
-class CSVDataset(Dataset):
-    def __init__(
-        self,
-        filepath: str,
-        train=True,
-        test_size=0.3,
-        shuffle=False,
-        random_state: float | None = None,
-    ) -> None:
-        super().__init__()
-
-        data = pd.read_csv(filepath)
-        X_train = data.iloc[:, :-1].to_numpy()
-        y_train = data.iloc[:, -1].to_numpy()
-
-        if test_size != 0.0:
-            X_train, X_test, y_train, y_test = train_test_split(
-                X_train,
-                y_train,
-                test_size=test_size,
-                shuffle=shuffle,
-                random_state=random_state,
-            )
-
-        if train:
-            X_train = torch.from_numpy(X_train.astype(np.float32))
-            y_train = torch.from_numpy(y_train.astype(np.float32))
-            idx = (y_train == 0).nonzero(as_tuple=False).squeeze()
-            self.X = X_train[idx]
-            self.y = y_train[idx]
-        else:
-            self.X = torch.from_numpy(X_test.astype(np.float32))
-            self.y = torch.from_numpy(y_test.astype(np.float32))
-
-    def __len__(self):
-        return len(self.X)
-
-    def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor]:
-        return self.X[idx], self.y[idx]
 
 
 def anomaly_score(X_pred: torch.Tensor, X: torch.Tensor) -> torch.Tensor:
@@ -142,13 +18,16 @@ def main():
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    n_features = 27
+    n_features = 29
+    hidden_size = 15
     learning_rate = 1e-3
     train_batch_size = 128
     test_batch_size = 64
 
+    data_path = "./data/ADBench/13_fraud.csv.gz"
+
     train_dataset = CSVDataset(
-        "./data/ADBench/1_ALOI.csv.gz",
+        data_path,
         train=True,
         test_size=0.2,
         shuffle=True,
@@ -156,7 +35,7 @@ def main():
     )
 
     test_dataset = CSVDataset(
-        "./data/ADBench/1_ALOI.csv.gz",
+        data_path,
         train=False,
         test_size=0.2,
         shuffle=True,
@@ -177,7 +56,7 @@ def main():
 
     auto_encoder = LSTMAutoEncoder(
         input_size=n_features,
-        hidden_size=54,
+        hidden_size=hidden_size,
         num_layers=5,
     )
 
@@ -187,7 +66,7 @@ def main():
     optimizer = optim.Adam(auto_encoder.parameters(), lr=learning_rate)
 
     # Training
-    epochs = 10
+    epochs = 5
     for epoch in range(epochs):
         print(f"epoch: {epoch}")
         for batch, (X, _) in enumerate(train_loader):
--- streamvigil/detectors/__init__.py
+++ streamvigil/detectors/__init__.py
@@ -1,3 +1,4 @@
 from ._basic import BasicAutoEncoder, BasicDetector
+from ._lstm import LSTMAutoEncoder
+from ._rapp import RAPP
 from ._rsrae import RSRAE
-from._rapp import RAPP

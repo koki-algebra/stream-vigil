7533b66de59bc2a2240c4d4a5c65b565e4e38024
koki-algebra <biz.kokiyamada1022@gmail.com>
2024-08-28 23:43:21 +0900

    fix ensemble vote

--- streamvigil/__init__.py
+++ streamvigil/__init__.py
@@ -1,3 +1,4 @@
 from ._dataset import ADBenchDataset, CSVDataset
+from ._ensemble_vote import EnsembleVoteModelPool
 from ._kde import ModelPoolWithKDE
 from ._performance_base import PerformanceBaseModelPool

ab8973cfad1c7f206ac65a030e1663edd78e0344
Koki Yamada <54975589+koki-algebra@users.noreply.github.com>
2024-09-04 17:45:12 +0900

    remove kde (#120)

--- streamvigil/__init__.py
+++ streamvigil/__init__.py
@@ -1,4 +1,3 @@
 from ._dataset import ADBenchDataset, CSVDataset
 from ._ensemble_vote import EnsembleVoteModelPool
-from ._kde import ModelPoolWithKDE
 from ._performance_base import PerformanceBaseModelPool

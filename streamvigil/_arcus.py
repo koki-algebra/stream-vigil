fcdb071c7bb152b847856035db50c12e6d5483e9
Koki Yamada <54975589+koki-algebra@users.noreply.github.com>
2024-09-04 19:37:11 +0900

    add arcus script (#121)

---
+++ streamvigil/_arcus.py
@@ -0,0 +1,51 @@
+from logging import getLogger
+
+from torch import Tensor
+
+from ._arcus_model_pool import ARCUSModelPool
+from .core._model import Model
+from .core._anomaly_detector import AnomalyDetector
+
+
+class ARCUS:
+    def __init__(
+        self,
+        detector: AnomalyDetector,
+        logger=getLogger(__name__),
+        reliability_threshold=0.95,
+        similarity_threshold=0.8,
+    ) -> None:
+        self.logger = logger
+        self.model_pool = ARCUSModelPool[Model](
+            detector,
+            reliability_threshold,
+            similarity_threshold,
+        )
+
+    def init(self):
+        # Add initial model
+        self.model_pool.add_model()
+
+    def stream_train(self, X: Tensor, is_logging = False):
+        if self.model_pool.is_drift():
+            self.logger.info("concept drift detected!")
+
+            # Add new model and train it
+            model_id = self.model_pool.add_model()
+            self.logger.info(f"new model {model_id} is added")
+
+            self.model_pool.stream_train(model_id, X)
+
+            # Compress model pool
+            if self.model_pool.compress(X, model_id):
+                self.logger.info("model pool compressed!")
+        else:
+            # Update the most reliable model
+            model_id = self.model_pool.find_most_reliable_model()
+            loss = self.model_pool.stream_train(model_id, X)
+
+            if is_logging:
+                self.logger.info(f"model {model_id} is selected: loss = {loss.item():0.5f}")
+
+    def predict(self, X: Tensor) -> Tensor:
+        return self.model_pool.predict(X)

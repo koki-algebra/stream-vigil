74537abc83c79bb6ef8a1e0d802f74af2b7a6dbc
Koki Yamada <54975589+koki-algebra@users.noreply.github.com>
2024-09-15 15:49:15 +0900

    fix model pool (#130)

--- scripts/experiments/proposed_method.py
+++ scripts/experiments/proposed_method.py
@@ -5,13 +5,15 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from yaml import safe_load
 
-from streamvigil.core import ModelPool
+from streamvigil.core import Model, ModelPool
 from streamvigil.detectors import BasicAutoEncoder, BasicDetector
 from streamvigil.utils import set_seed
 
 train_batch_size = 128
 test_batch_size = 64
 epochs = 5
+
+INIT_BATCHES = 10
 
 
 def main():
@@ -44,16 +46,7 @@ def main():
     detector = BasicDetector(auto_encoder)
 
     # Model Pool
-    model_pool = ModelPool(detector)
-
-    print("current_model:", model_pool.current_model_id)
-
-    # Train initial model
-    logger.info("start training the initial model")
-    for batch, (X, _) in enumerate(train_loader):
-        X = X.view(X.size(0), -1)
-        model_pool.stream_train(X)
-    logger.info("finish training initial model")
+    model_pool = ModelPool[Model](detector)
 
     # Training
     for epoch in range(epochs):
@@ -61,8 +54,29 @@ def main():
         for batch, (X, _) in enumerate(train_loader):
             X = X.view(X.size(0), -1)
 
-            model_pool.stream_train(X)
             model_pool.update_window(X)
+
+            current_model = model_pool.get_model(model_pool.current_model_id)
+
+            if current_model.num_batches > INIT_BATCHES:
+                # Concept Drift detection
+                if current_model.is_drift():
+                    logger.info("concept drift detected!")
+
+                    adapted_model_id = model_pool.find_adapted_model()
+                    if adapted_model_id is not None:
+                        model_pool.current_model_id = adapted_model_id
+
+                        logger.info(f"find adapted model: {adapted_model_id}")
+                    else:
+                        # Add new model
+                        new_model_id = model_pool.add_model()
+                        model_pool.current_model_id = new_model_id
+
+                        logger.info(f"add new model: {new_model_id}")
+
+            # Train current model
+            model_pool.stream_train(X)
 
 
 if __name__ == "__main__":

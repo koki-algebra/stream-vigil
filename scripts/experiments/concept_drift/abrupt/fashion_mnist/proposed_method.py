49dda178be917269f016649dbd8ae373a8a29a1f
koki-algebra <biz.kokiyamada1022@gmail.com>
2024-10-28 23:58:08 +0900

    fix script

--- scripts/experiments/concept_drift/abrupt/fashion_mnist/proposed_method.py
+++ scripts/experiments/concept_drift/abrupt/fashion_mnist/proposed_method.py
@@ -4,12 +4,13 @@ from logging.config import dictConfig
 import matplotlib.pyplot as plt
 import numpy as np
 from torch.utils.data import DataLoader
+from torcheval.metrics import BinaryAUPRC, BinaryAUROC
 from torchvision import datasets, transforms
 from yaml import safe_load
 
 from streamvigil.core import Model, ModelPool
 from streamvigil.detectors import BasicAutoEncoder, BasicDetector
-from streamvigil.utils import filter_index, set_seed
+from streamvigil.utils import filter_index, set_seed, to_anomaly_labels
 
 RANDOM_STATE = 80
 TRAIN_BATCH_SIZE = 128
@@ -45,6 +46,15 @@ def main():
         normal_labels=[3, 4],
         anomaly_labels=[7, 8, 9],
     )
+    train_dataset.targets[concept_a_idx] = to_anomaly_labels(
+        train_dataset.targets[concept_a_idx],
+        normal_labels=[1, 2],
+    )
+    train_dataset.targets[concept_b_idx] = to_anomaly_labels(
+        train_dataset.targets[concept_b_idx],
+        normal_labels=[3, 4],
+    )
+
     result_idx = concept_a_idx + concept_b_idx
     train_dataset.data = train_dataset.data[result_idx]
     train_dataset.targets = train_dataset.targets[result_idx]
@@ -70,6 +80,9 @@ def main():
         window_gap=500,
     )
 
+    auroc = BinaryAUROC()
+    auprc = BinaryAUPRC()
+
     losses = []
     detected = []
 
@@ -80,6 +93,11 @@ def main():
         model_pool.update_window(X)
 
         current_model = model_pool.get_model(model_pool.current_model_id)
+
+        scores = current_model.predict(X)
+
+        auroc.update(scores, y)
+        auprc.update(scores, y)
 
         if current_model.num_batches > INIT_BATCHES:
             # Concept Drift detection
@@ -112,6 +130,9 @@ def main():
     detected = np.array(detected)
 
     fig, ax1 = plt.subplots(figsize=(12, 6))
+
+    print(f"AUROC: {auroc.compute():0.5f}")
+    print(f"AUPRC: {auprc.compute():0.5f}")
 
     ax1.plot(losses, color=LOSS_COLOR, label="Losses")
     ax1.set_xlabel("Iterations")
